881fe456884c8ae4bf4a23ebe06c30b0929a0c2e
charleslamb212 <charlesandrewlamb@gmail.com>
2023-02-07 19:25:46 -0800

    completed insertion sort

--- sort.py
+++ sort.py
@@ -8,7 +8,16 @@ def is_sorted(ls):
 
 def insertion_sort(li):
     # implement your algorithm here
-    pass
+    # create insertion sort algorithm
+    for i in range(1, len(li)): # start from 1 because 0 is already sorted
+        key = li[i] # key is the value we want to insert
+        j = i - 1 # j is the index of the last element in the sorted array
+        while j >= 0 and key < li[j]: # while j is not out of range and key is smaller than the last element in the sorted array
+            li[j + 1] = li[j] # move the last element in the sorted array to the right
+            j -= 1 # move j to the left
+        li[j + 1] = key # insert the key to the right of j
+
+
 
 
 # for testing
@@ -18,3 +27,4 @@ li = [44, 41, 35, 34, 7, 8, 44, 38, 28, 44, 16, 31, 13, 31, 42, 19, 2, 47, 32, 1
 insertion_sort(li)
 # should return True, because insertion sort is an in-place sort
 print(is_sorted(li))
+print("Sorted list:", li)

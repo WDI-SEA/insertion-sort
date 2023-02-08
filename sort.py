ff0435729465898135f031faacc71a2ed2e740ac
ninambash <ninambash@gmail.com>
2023-02-08 09:23:37 -0800

    updated

--- sort.py
+++ sort.py
@@ -5,16 +5,29 @@ def is_sorted(ls):
             return False
     return True
 
+def insertion_sort(li, n=None):
+    if n is None:
+        n = len(li)
+    if n <= 1:
+        return
+    insertion_sort(li, n - 1)
+    last = li[n - 1]
+    j = n - 2
+    while j >= 0 and li[j] > last:
+        li[j + 1] = li[j]
+        j -= 1
+    li[j + 1] = last
+    return li
 
-def insertion_sort(li):
-    # implement your algorithm here
-    pass
+li = [44, 41, 35, 34, 7, 8, 44, 38, 28, 44, 16, 31, 13, 31, 42, 19, 2, 47, 32, 17, 14, 27, 30, 4,      41, 39, 37, 30, 42, 43, 10, 3, 48, 16, 11, 47, 9, 40, 22, 23, 9, 2, 35, 6, 7, 5, 45, 42, 24, 49]
+
+insertion_sort(li)
+print(li)
+
 
 
 # for testing
 li = [44, 41, 35, 34, 7, 8, 44, 38, 28, 44, 16, 31, 13, 31, 42, 19, 2, 47, 32, 17, 14, 27, 30, 4,
       41, 39, 37, 30, 42, 43, 10, 3, 48, 16, 11, 47, 9, 40, 22, 23, 9, 2, 35, 6, 7, 5, 45, 42, 24, 49]
-
-insertion_sort(li)
 # should return True, because insertion sort is an in-place sort
-print(is_sorted(li))
+ 

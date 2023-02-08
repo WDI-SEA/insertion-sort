ed645a7e581569a1471308677021ad7d034bd5e4
Crystal S <117321332+Turtle-Sprite@users.noreply.github.com>
2023-02-07 19:39:57 -0800

    finished.

--- sort.py
+++ sort.py
@@ -7,13 +7,22 @@ def is_sorted(ls):
 
 
 def insertion_sort(li):
-    # implement your algorithm here
-    pass
+    for i in range(1, len(li)):
+        j = i 
+        # 4
+        while li[j - 1] > li[j] and j > 0: # 35 > 34?
+            li[j - 1], li[j] = li[j], li[j - 1] # [34, 35, 41, 44, 7, 8,
+            j -= 1 # j = 0
 
-
+    # for i in range(len(li) -1):
 # for testing
-li = [44, 41, 35, 34, 7, 8, 44, 38, 28, 44, 16, 31, 13, 31, 42, 19, 2, 47, 32, 17, 14, 27, 30, 4,
-      41, 39, 37, 30, 42, 43, 10, 3, 48, 16, 11, 47, 9, 40, 22, 23, 9, 2, 35, 6, 7, 5, 45, 42, 24, 49]
+li = [44, 41, 35, 34, 7, 8, 44, 38, 28, 44, 16, 31, 13, 31, 42, 19, 2, 47, 32, 17, 14, 27, 30, 4, 41, 39, 37, 30, 42, 43, 10, 3, 48, 16, 11, 47, 9, 40, 22, 23, 9, 2, 35, 6, 7, 5, 45, 42, 24, 49]
+
+
+
+
+
+
 
 insertion_sort(li)
 # should return True, because insertion sort is an in-place sort

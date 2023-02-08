d5275aa910633d456c60ff9d705e29e365fae58d
curtlen88 <curtlen88@gmail.com>
2023-02-07 21:15:27 -0700

    curtlen aumiller submittal

--- sort.py
+++ sort.py
@@ -8,8 +8,20 @@ def is_sorted(ls):
 
 def insertion_sort(li):
     # implement your algorithm here
-    pass
-
+    # loop through the list
+    for i in range(1, len(li)):
+        # set the key to the current index
+        key = li[i]
+        # set j to the index before the current index
+        j = i - 1
+        # while j is greater than or equal to 0 and the key is less than the value at index j
+        while j >= 0 and key < li[j]:
+            # set the value at index j + 1 to the value at index j
+            li[j + 1] = li[j]
+            # decrement j
+            j -= 1
+        # set the value at index j + 1 to the key
+        li[j + 1] = key
 
 # for testing
 li = [44, 41, 35, 34, 7, 8, 44, 38, 28, 44, 16, 31, 13, 31, 42, 19, 2, 47, 32, 17, 14, 27, 30, 4,

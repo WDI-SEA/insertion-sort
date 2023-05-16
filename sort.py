563682d368261ac3e6c730af2baaab713b90a731
juanedcabrera <juanedcabrera@gmail.com>
2023-05-15 19:55:17 -0600

    First Answer

--- sort.py
+++ sort.py
@@ -5,16 +5,26 @@ def is_sorted(ls):
             return False
     return True
 
+def swap(i, j):
+    li2[i], li2[j] = li2[j], li2[i]
 
-def insertion_sort(li):
-    # implement your algorithm here
-    pass
+def insertion_sort(li2):
+    for i in range(0, len(li2)):
+        print(f'this is for loop: {li2}')
+        j = i
+        while j > 0 and li2[j] < li2[j - 1]:
+            swap(j, j - 1)
+            j -= 1
+            print(f'this is while loop: {li2}')
 
 
 # for testing
 li = [44, 41, 35, 34, 7, 8, 44, 38, 28, 44, 16, 31, 13, 31, 42, 19, 2, 47, 32, 17, 14, 27, 30, 4,
       41, 39, 37, 30, 42, 43, 10, 3, 48, 16, 11, 47, 9, 40, 22, 23, 9, 2, 35, 6, 7, 5, 45, 42, 24, 49]
 
-insertion_sort(li)
+li2 = [12, 1, 32, 23, 21, 8]
+
+insertion_sort(li2)
+print(li2)
 # should return True, because insertion sort is an in-place sort
-print(is_sorted(li))
+print(is_sorted(li2))

7468382e6391fb8a1843bf7791d3d4583129afd0
Heiner000 <garrett.heiner@gmail.com>
2023-05-15 22:33:56 -0700

    not quite there yet

--- sort.py
+++ sort.py
@@ -7,13 +7,16 @@ def is_sorted(ls):
 
 
 def insertion_sort(li):
-    # implement your algorithm here
+    # iterate over set - start with arr[0]
+    # for each i in arr
+        # key = arr[i]
+        # if arr[i] > arr[i + 1]:
+            # arr[i] = arr[i + 1]
     pass
 
 
 # for testing
-li = [44, 41, 35, 34, 7, 8, 44, 38, 28, 44, 16, 31, 13, 31, 42, 19, 2, 47, 32, 17, 14, 27, 30, 4,
-      41, 39, 37, 30, 42, 43, 10, 3, 48, 16, 11, 47, 9, 40, 22, 23, 9, 2, 35, 6, 7, 5, 45, 42, 24, 49]
+li = [44, 41, 35, 34, 7, 8, 44, 38, 28, 44, 16, 31, 13, 31, 42, 19, 2, 47, 32, 17, 14, 27, 30, 4, 41, 39, 37, 30, 42, 43, 10, 3, 48, 16, 11, 47, 9, 40, 22, 23, 9, 2, 35, 6, 7, 5, 45, 42, 24, 49]
 
 insertion_sort(li)
 # should return True, because insertion sort is an in-place sort
